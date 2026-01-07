608522d95bdfe4298193721ab8f04e6041ff8543
my-code-library <mrscottwebber@gmail.com>
2026-01-07 10:47:01 -0800

    Update module-loader.php

--- includes/module-loader.php
+++ includes/module-loader.php
@@ -33,12 +33,11 @@ class PJ_Module_Loader {
         }
 
         if ( get_option('pj_enable_magic_link_login', '1') ) {
-            $modules[] = 'auth/magic-links.php';
+            $modules = [
+                'auth/magic-links.php',
+                'ui/passwordless-toggle.php',
+            ];
         }
-        if ( get_option('pj_enable_magic_link_login', '1') ) {
-            $modules[] = 'ui/passwordless-toggle.php';
-        }        
-
         // Loop through and load modules
         foreach ($modules as $module) {
             $path = $base_dir . $module;
@@ -51,6 +50,7 @@ class PJ_Module_Loader {
 }
 
 add_action('plugins_loaded', ['PJ_Module_Loader', 'load']);
+
 
 
 
