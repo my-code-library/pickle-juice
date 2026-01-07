831d4ec7790b06f6052a42a03778a03c0587752a
my-code-library <mrscottwebber@gmail.com>
2026-01-07 10:30:13 -0800

    Update passwordless-toggle.php

--- modules/ui/passwordless-toggle.php
+++ modules/ui/passwordless-toggle.php
@@ -12,7 +12,7 @@ define('PJ_PASSWORDLESS_TOGGLE_LOADED', true);
 
 add_action('login_form', function() {
 
-    if (get_option('pj_disable_password_login') !== '1') {
+   if ( get_option('pj_enable_magic_link_login', '1') ) {
         return;
     }
 
